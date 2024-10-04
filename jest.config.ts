f2eb50a8220db1fe266a7b87648149cf1fe9f8ef
Anton Andreev <rawkward@yandex.ru>
2024-10-05 02:40:41 +0300

    changed jest coverage settings

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,9 @@ const config: JestConfigWithTsJest = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/services/**/*.{ts,tsx}'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
